8043e1589c8d9cbaf798c4b180dde9be080ec3dc
moshebeutel <moshebeutel@gmail.com>
2025-07-13 15:00:26 +0300

    normalize kernel pca

--- simplegep/embeddings/kernel_pca_embedder.py
+++ simplegep/embeddings/kernel_pca_embedder.py
@@ -2,6 +2,7 @@ from typing import Optional
 import torch
 from sklearn.decomposition import KernelPCA
 from simplegep.embeddings.embedder import Embedder
+from simplegep.embeddings.utils import normalize_return_transform
 
 
 class KernelPCAEmbedder(Embedder):
@@ -12,20 +13,25 @@ class KernelPCAEmbedder(Embedder):
         self._gamma = gamma
         self._kernel_pca = KernelPCA(n_components=self._num_basis_elements, kernel=self._kernel_type, gamma=self._gamma,
                                      fit_inverse_transform=True)
+        self._center = None
+        self._scale = None
 
     def calc_embedding_space(self, data: torch.Tensor):
+        normalized_x, self._center, self._scale = normalize_return_transform(data)
         data_np = data.detach().cpu().numpy()
         self._kernel_pca.fit(data_np)
 
     def embed(self, src: torch.Tensor) -> torch.Tensor:
-        src_np = src.detach().cpu().numpy()
-        embedding: torch.Tensor = torch.from_numpy(self._kernel_pca.transform(src_np))
+        normalized = (src - self._center) / self._scale
+        normalized_np = normalized.detach().cpu().numpy()
+        embedding: torch.Tensor = torch.from_numpy(self._kernel_pca.transform(normalized_np))
         return embedding
 
     def project_back(self, embedding: torch.Tensor) -> torch.Tensor:
         embedding_np = embedding.detach().cpu().numpy()
         reconstructed: torch.Tensor = torch.from_numpy(self._kernel_pca.inverse_transform(embedding_np))
-        return reconstructed
+        reconstructed_transformed = (reconstructed * self._scale) + self._center
+        return reconstructed_transformed
 
 
 # if __name__ == '__main__':
